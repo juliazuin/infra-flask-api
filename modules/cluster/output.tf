290759f9fd978b9dd348f26ad86ea8e2d4399381
Julia Zuin <zuinjulia@hotmail.com>
2026-01-07 19:05:38 -0300

    feat: first commit

---
+++ modules/cluster/output.tf
@@ -0,0 +1,19 @@
+output "cluster_name" {
+  value = aws_eks_cluster.eks_cluster.id
+}
+
+output "oidc" {
+  value = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+}
+
+output "certificate_authority" {
+  value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.eks_cluster.endpoint
+}
+
+output "cluster_sg" {
+  value = aws_eks_cluster.eks_cluster.vpc_config[0].cluster_security_group_id
+}
